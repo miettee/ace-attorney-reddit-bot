59332a2b6fbddfd0b9ccdddcd2051297d9f07d92
mie <76075501+miettee@users.noreply.github.com>
2021-01-24 22:12:11 +0000

    Update and rename auth.py to launcher.py

---
+++ launcher.py
@@ -0,0 +1,24 @@
+import discord
+import dotenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='!')
+
+initial_extensions = (
+    'cogs.help',
+    'cogs.make_scene'
+
+)
+
+if __name__ == '__main__':
+
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+
+bot.run(token)
+
